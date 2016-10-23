e99c8660d7421f0d963e95f18b9c79fce55971a0
rtracey <robert.w.tracey@gmail.com>
2016-10-23 13:58:53 -0300

    Added Serializable Data
    Data added for cut scenes (in-game and in-between levels) and player
    stats.

---
+++ Assets/Scripts/Serializable/CutSceneEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class CutSceneEvent{
+
+	public AudioClip sound;
+	public String dialogue;
+	public bool isSkippable;
+	public float duration;
+}
---
+++ Assets/Scripts/Serializable/Stats.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class Stats {
+
+	public Level currentLevel;
+
+	public int score;
+	public int packagesDelivered;
+	public int damageTaken;
+	public int gameplayTime;
+	public int numberRespawns;
+	public int numberPowerUps;
+
+}
