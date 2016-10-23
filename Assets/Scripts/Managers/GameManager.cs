8d3918e90786f7c1725fc6ad3c825a4a975e2b44
rtracey <robert.w.tracey@gmail.com>
2016-10-23 14:01:35 -0300

    Files for Game Managers
    Manager scripts not yet started. These are just placeholders.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	public Stats stats;
+
+	void Initialize()
+	{
+		if (instance != null) 
+			DestroyImmediate (instance);
+
+		instance = this;
+	}
+
+}
