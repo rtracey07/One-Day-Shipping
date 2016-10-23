3ca42d6817902f8fbbbe99ad9f716ce4bd541d09
Mons1Oerjan <mons@dal.ca>
2016-10-23 15:17:12 -0300

    Added GameClockManager script

---
+++ Assets/Scripts/Managers/GameClockManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameClockManager : MonoBehaviour {
+
+	public static GameClockManager instance;
+
+	public float time;
+
+	public bool freeze;
+
+	void Awake()
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		}
+
+		if (instance != this) 
+		{
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+
+		freeze = false;
+	}
+
+	void Update()
+	{
+		if (!freeze) {
+			time = Time.deltaTime;
+		}
+	}
+
+}
