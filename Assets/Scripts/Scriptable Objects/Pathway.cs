07edb1eea36dbffd1a8b29270b70b8b96a0a18fc
johnalatouf <johnalatouf@gmail.com>
2016-11-15 22:08:36 -0400

    taxi can be put in different regions
    taxi prefab is made, need to make a few other vehicle prefabs

---
+++ Assets/Scripts/Scriptable Objects/Pathway.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "Data/Pathway")]
+public class Pathway : ScriptableObject {
+
+	public List<Vector3> pathway;
+
+}
+
