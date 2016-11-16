101b86654b8ea02232fa847ba1391a138cac6d61
rtracey <robert.w.tracey@gmail.com>
2016-11-15 21:34:14 -0400

    Adding Package Selection to Scriptable Objects and Managers

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	public Level levelData;
+	public GameObject currentPickup;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		currentPickup = levelData.GetPickupLocation ();
 	}
 }
--- Assets/Scripts/Scriptable Objects/Level.cs
+++ Assets/Scripts/Scriptable Objects/Level.cs
@@ -1,8 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 [CreateAssetMenu(menuName = "Data/Level")]
 public class Level : ScriptableObject{
 
 	public string name;
+
+	[HideInInspector]
+	[Serializable]
+	public class PickupLocationGroup
+	{
+		public List<string> locations;
+	}
+		
+	public List<PickupLocationGroup> pickupLocations;
+
+	private int currIndex = 0;
+
+	public GameObject GetPickupLocation()
+	{
+		string current = pickupLocations [currIndex].locations [UnityEngine.Random.Range(0, pickupLocations [currIndex].locations.Count-1)];
+		GameObject location = GameObject.Find (current);
+		return location;
+	}
 }
