18c533f9723aeee2112ac7058bd755bba994a456
rtracey <robert.w.tracey@gmail.com>
2016-11-16 23:56:26 -0400

    Added a Pathway Creation Editor Window

---
+++ Assets/Scripts/Utility/PathHelper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+
+[ExecuteInEditMode]
+public class PathHelper : MonoBehaviour {
+
+	public Pathway pathway;
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawWireSphere (this.transform.position, 1);
+
+		if (pathway != null)
+			iTween.DrawPath (pathway.pathway.ToArray ());
+	}
+}
