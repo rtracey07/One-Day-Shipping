ad012b2cfb41151547e7b54effc3c23dea55b5f1
rtracey <robert.w.tracey@gmail.com>
2016-11-17 20:21:40 -0400

    Updated location scripts to include drop off and pickup locations

--- Assets/Scripts/GameObjects/PickupLocation.cs
+++ Assets/Scripts/GameObjects/PickupLocation.cs
@@ -1,34 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PickupLocation : MonoBehaviour {
+public class PickupLocation : Location {
 
-	private bool selectedLocation;
-	public Animator m_Animator;
-
-	public ParticleSystem activeEffect;
-	public ParticleSystem collectedEffect;
-
-
-	// Use this for initialization
-	void Start () {
-		selectedLocation = false;
-	}
-
-	public void SetActive()
-	{
-		selectedLocation = true;
-		activeEffect.Play ();
-		m_Animator.SetBool ("CollectedBox", false);
-		m_Animator.SetBool ("SelectedBox", true);
-	}	
-
-	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Player") {
-			GameManager.Instance.hasPackage = true;
-			activeEffect.Stop ();
-			m_Animator.SetBool ("CollectedBox", true);
-			m_Animator.SetBool ("SelectedBox", false);
-		}
-	}
 }
