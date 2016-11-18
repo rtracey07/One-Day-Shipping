ddeb52b1dc4c3d0717706a8982d3a7a4842dba40
rtracey <robert.w.tracey@gmail.com>
2016-11-17 20:22:32 -0400

    Updated level scriptable object to include pickup and drop off locations

--- Assets/Scripts/Scriptable Objects/Level.cs
+++ Assets/Scripts/Scriptable Objects/Level.cs
@@ -10,29 +10,53 @@ public class Level : ScriptableObject{
 
 	[HideInInspector]
 	[Serializable]
-	public class PickupLocationGroup
+	public class LocationGroup
 	{
 		public List<string> locations;
 	}
 
-	public List<PickupLocationGroup> pickupLocations;
+	public List<LocationGroup> pickupLocations;
+	public List<LocationGroup> dropoffLocations;
 
 	private int currIndex = 0;
 
-	public PickupLocation GetPickupLocation(ref PickupLocation[] activeLocations)
+	public Location GetPickupLocation(ref Location[] activeLocations)
 	{
 		string current = pickupLocations [currIndex].locations [UnityEngine.Random.Range(0, pickupLocations [currIndex].locations.Count)];
 
 
 		for (int i = 0; i < activeLocations.Length; i++) {
 			if (activeLocations[i].gameObject.name == current) {
-				PickupLocation location = activeLocations [i];
+				Location location = activeLocations [i];
 				location.SetActive ();
 				return location;
 			}
 		}
 
-		Debug.Log (string.Format ("PickupLocation {0} not found in scene.", current));
+		Debug.Log (string.Format ("Pickup Location {0} not found in scene.", current));
+		return null;
+	}
+
+	public Location GetDropoffLocation(ref Location[] activeLocations)
+	{
+		string current = dropoffLocations [currIndex].locations [UnityEngine.Random.Range(0, pickupLocations [currIndex].locations.Count)];
+		currIndex++;
+
+		for (int i = 0; i < activeLocations.Length; i++) {
+			if (activeLocations[i].gameObject.name == current) {
+				Location location = activeLocations [i];
+				location.SetActive ();
+				return location;
+			}
+		}
+
+		Debug.Log (string.Format ("Dropoff Location {0} not found in scene.", current));
+
+		currIndex++;
+
+		if (currIndex >= pickupLocations.Count)
+			currIndex = 0;
+		
 		return null;
 	}
 }
