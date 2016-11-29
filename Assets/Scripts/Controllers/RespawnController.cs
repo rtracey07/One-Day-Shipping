a3baa75917b6c2385108a7d9da521582529c21b3
Mons1Oerjan <mons@dal.ca>
2016-11-29 00:23:05 -0400

    Added respawn on water collision
    still needs to be done:
    - destroy package on water collision

---
+++ Assets/Scripts/Controllers/RespawnController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnController : MonoBehaviour {
+
+	Transform start;
+	public AudioClip respawnSound;
+
+	void Start(){
+		start = GameObject.FindGameObjectWithTag("SpawnPoint").transform;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Water"){
+			AudioSource.PlayClipAtPoint (respawnSound, GameManager.Instance.mainCamera.transform.position);
+			transform.position = start.position;
+		}
+	}
+}
