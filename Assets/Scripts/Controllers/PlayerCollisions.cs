7da50682dafbe8ca1318d2d77d80fc33a8f158e5
Mons1Oerjan <mons@dal.ca>
2016-11-29 16:47:37 -0400

    adding more SFX to resources/audio

---
+++ Assets/Scripts/Controllers/PlayerCollisions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisions : MonoBehaviour {
+
+	public AudioClip treeCollision;
+
+
+	void OnCollisionEnter(Collider other){
+	
+		switch (other.gameObject.tag) {
+
+		case "tree":
+			AudioManager.Instance.PlaySoundEffect (treeCollision);
+			break;
+
+		}
+	
+	}
+}
