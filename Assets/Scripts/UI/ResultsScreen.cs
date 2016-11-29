9918a10c78f2c9eb8db673a33fab12c1d23f8abe
rtracey <robert.w.tracey@gmail.com>
2016-11-28 21:50:03 -0400

    Results screen implementation v2

--- Assets/Scripts/UI/ResultsScreen.cs
+++ Assets/Scripts/UI/ResultsScreen.cs
@@ -12,6 +12,7 @@ public class ResultsScreen : MonoBehaviour {
 	public Text postmen;
 	public Text total;
 
+	private int totalVal;
 
 	// Use this for initialization
 	void Start () {
@@ -20,21 +21,49 @@ public class ResultsScreen : MonoBehaviour {
 
 	IEnumerator TallyScore()
 	{
-		float time = 0.0f;
+
+		yield return new WaitForSeconds (1.0f);
+
 		int currScore = GameManager.Instance.stats.packagesDelivered * 100;
-		int totalVal = currScore;
+		totalVal = 0;
+
+		yield return StartCoroutine (Tally (currScore, delivered, total));
+
+		currScore = GameManager.Instance.stats.packagesDestroyed * -100;
+		yield return StartCoroutine (Tally (currScore, destroyed, total));
+
+		currScore = GameManager.Instance.stats.carsHit * -10;
+		yield return StartCoroutine (Tally (currScore, cars, total));
+
+		currScore = GameManager.Instance.stats.dogsHit * -20;
+		yield return StartCoroutine (Tally (currScore, dogs, total));
+
+		currScore = GameManager.Instance.stats.postmenHit * -10;
+		yield return StartCoroutine (Tally (currScore, postmen, total));
+
+		float time = 0.0f;
+		float originalFontSize = total.fontSize;
+	}
+
+
+
+	IEnumerator Tally(int currScore, Text currText, Text currTotalText)
+	{
+		float time = 0.0f;
+		float currTotal = totalVal;
+		totalVal += currScore;
+
+		if(currScore < 0)
+			currText.color = Color.red;
 
 		while (time <= tallyLength) {
-			delivered.text = ((int)(Mathf.Lerp (0, currScore, time / tallyLength))).ToString ();
-			total.text = ((int)(Mathf.Lerp (0, totalVal, time / tallyLength))).ToString ();
+			currText.text = ((int)(Mathf.Lerp (0, currScore, time / tallyLength))).ToString ();
+			currTotalText.text = ((int)(Mathf.Lerp (currTotal, totalVal, time / tallyLength))).ToString ();
 			time += GameClockManager.Instance.time;
 			yield return null;
 		}
 
-		delivered.text = currScore.ToString();
-		total.text = currScore.ToString();
-
-		time = 0.0f;
-
+		currText.text = currScore.ToString();
+		currTotalText.text = totalVal.ToString();
 	}
 }
