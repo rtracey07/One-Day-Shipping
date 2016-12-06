32a87cb2855c9a37c66a57608d8807e1b8e7fc26
rtracey <robert.w.tracey@gmail.com>
2016-12-06 15:36:30 -0400

    Light adjustment script for last level

---
+++ Assets/LightLevelAdjust.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightLevelAdjust : MonoBehaviour {
+
+	private Light m_MainLight;
+
+	[Range(-1,1)]
+	public float adjustment;
+
+	void Start () {
+		m_MainLight = GameObject.Find ("Sun").GetComponent<Light> ();
+		m_MainLight.intensity += adjustment;
+	}
+
+
+}
