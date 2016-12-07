e9eb5bb76cf650732811d413b1fcad0689dacc2d
Matt <mattyfar@live.com>
2016-12-06 20:01:26 -0400

    Adding comments to Utility

--- Assets/Scripts/Utility/LightLevelAdjust.cs
+++ Assets/Scripts/Utility/LightLevelAdjust.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * The MonoBehaviour script for adjusting the level lighting
+ */
 public class LightLevelAdjust : MonoBehaviour {
 
 	private Light m_MainLight;
@@ -8,10 +11,12 @@ public class LightLevelAdjust : MonoBehaviour {
 	[Range(-1,1)]
 	public float adjustment;
 
+    /**
+     * Sets the light intensity
+     */
 	void Start () {
 		m_MainLight = GameObject.Find ("Sun").GetComponent<Light> ();
 		m_MainLight.intensity += adjustment;
 	}
-
 
 }
