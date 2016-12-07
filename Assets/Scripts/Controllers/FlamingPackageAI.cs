09a9e61d47f0b81a2bcdaf20dd268ab93d0a100e
Mons1Oerjan <mons@dal.ca>
2016-12-06 21:40:16 -0400

    final commit

--- Assets/Scripts/Controllers/FlamingPackageAI.cs
+++ Assets/Scripts/Controllers/FlamingPackageAI.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// Flaming package AI.
+/// </summary>
 public class FlamingPackageAI : MonoBehaviour {
 
 	private List<GameObject> flamingPackages;
@@ -15,22 +18,30 @@ public class FlamingPackageAI : MonoBehaviour {
 
 	private GameObject pack;
 
+	/// <summary>
+	/// At instantiation
+	/// </summary>
 	void Start(){
 		
 		//play sound when object gets created:
 		if (fallSound != null){
 			AudioSource.PlayClipAtPoint (fallSound, this.gameObject.transform.position);
 		}
-		StartCoroutine (DestroyAfterFiveSeconds ());
+		StartCoroutine (DestroyAfterTenSeconds ());
 
 	}
 
+	/// <summary>
+	/// Raises the collision enter event.
+	/// </summary>
+	/// <param name="other">Other.</param>
 	void OnCollisionEnter(Collision other){
 		//play collision sound:
 		AudioManager.Instance.PlaySoundEffect(collisionSound, 0.05f);
 
 		if (other.gameObject.tag == "Player") {
 			//check if he has a package and damage it approprietly:
+			//play random damageSound:
 			switch(Random.Range(1,3)){
 			case 1:
 				AudioManager.Instance.PlaySoundEffect (damageSound1);
@@ -43,20 +54,24 @@ public class FlamingPackageAI : MonoBehaviour {
 				break;
 			}
 
-			//get the package and do damage
+			//get the package and do damage:
 			pack = GameObject.Find ("Package");
 			if (pack != null) {
 				Debug.Log ("Package Hit");
 				pack.GetComponent<Package> ().DamagePackage (damageStrength);
 
 			}
-			StopCoroutine ("DestroyAfterFiveSeconds");
+			//stop coroutine on collision:
+			StopCoroutine ("DestroyAfterTenSeconds");
 			Destroy (this.gameObject);
 
 		}
 			
 	}
 
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
 	void Update(){
 		if (GameClockManager.Instance.freeze) {
 			gameObject.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
@@ -65,7 +80,11 @@ public class FlamingPackageAI : MonoBehaviour {
 		}
 	}
 
-	public IEnumerator DestroyAfterFiveSeconds(){
+	/// <summary>
+	/// Destroy package 10 seconds after spawn.
+	/// </summary>
+	/// <returns>The after five seconds.</returns>
+	public IEnumerator DestroyAfterTenSeconds(){
 		yield return new WaitForSeconds (10.0f);
 		if (this.gameObject != null) {
 			Destroy (this.gameObject);
